c5d84e2411bbdf910522a7b7e597a340619f4b72
esquinas <esquinas.enrique@gmail.com>
2016-12-13 17:48:27 +0100

    Write return values in comments for 2nd iteration of [1, 1, 2].

--- distinct_permutation.rb
+++ distinct_permutation.rb
@@ -13,14 +13,31 @@ class Array
 
     loop do
       # from: "The Art of Computer Programming" by Donald Knuth.
-      j = size - 2; #=> j = 3 - 2; j #=> 1.
+      j = size - 2; #=> j = 3 - 2; j #=> 1.  2nd=> j #=> 1.
       j -= 1 while j > 0 && copy[j] >= copy[j + 1] #=> false ; j #=> 1.
-      break if copy[j] >= copy[j + 1] #=> break if 1 >= 2 #=> false.
+      # 2nd=> copy #=> [1, 2, 1]; true ; j -= 1 ; false ; j #=> 0.
+      break if copy[j] >= copy[j + 1] #=> break if 1 >= 2 #=> continue.
+      # 2nd=> break if 1 >= 2 #=> continue.
       l = size - 1 #=> l = 3 - 1; l #=> 2.
+      # 2nd=> l #=> 2
       l -= 1 while copy[j] >= copy[l] # 1 >= 2 #=> false ; l #=> 2.
+      # 2nd=> 1 >= 1 #=> true ; l #=> 1 ; 1 >= 2 ; false ; l #=> 1.
       copy[j], copy[l] = copy[l], copy[j] # copy #=> [1, 2, 1].
-      copy[j + 1..-1] = copy[j + 1..-1].reverse # copy #=> [1, 2, 1].
+      # 2nd=> copy #=> [2, 1, 1].
+      copy[j + 1..-1]  = copy[j + 1..-1].reverse # copy #=> [1, 2, 1].
+      # 2nd=> copy #=> [2, 1, 1].
       yield copy.dup #=> [1, 2, 1].
+      #            2nd=> [2, 1, 1].
     end
   end
 end
+
+__END__
+
+## Yield history: ##
+
+```ruby
+[1, 1, 2] # 0th iteration.
+[1, 2, 1] # 1st   "
+[2, 1, 1] # 2nd   "
+```
